f49cce460fbb4b72cd750f57d9a15070de15b600
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-13 11:47:38 +0100

    Exercise 1x5 done

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,7 +14,7 @@ const fourthTeacher = teachers.at(3);
 console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4, 1, "Patrick")
+teachers.splice(4, 1, "Patrick");
 console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
@@ -26,6 +26,8 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
