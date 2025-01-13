31eab073adae03558f0ccd8b260d2e1f763d4f5f
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-13 12:21:37 +0100

    Exercise 2x4 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,7 +11,7 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers)
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -26,12 +26,18 @@ for (i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(teachers.indexOf("Ed"), 1)
-console.log(teachers)
+teachers.splice(teachers.indexOf("Ed"), 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf("Fabio");
+
+if (isFabioPresent == 0) {
+  console.log("Fabio non è presente nell'array.")
+} else {
+  console.log("Fabio è presente nell'array.")
+}
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
